a286566f55e80dbd18e1cbf313dc380a9a5156a9
zhongmingkun <v-zhongmingkun@meizu.com>
2019-02-20 17:13:21 +0800

    feat: 改用multi_image_picker

---
+++ hello_world/android/app/src/main/java/com/vitanov/multiimagepickerexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.vitanov.multiimagepickerexample;
+
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.vitanov.multiimagepicker.FileDirectory;
+
+import java.io.File;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
--- hello_world/lib/pages/use_storage_img.dart
+++ hello_world/lib/pages/use_storage_img.dart
@@ -1,188 +1,79 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:multiple_image_picker/multiple_image_picker.dart';
-import 'package:video_player/video_player.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+import 'asset_view.dart';
 
 class UseStorageImgPage extends StatefulWidget {
-
   @override
   _PageState createState() => new _PageState();
 }
 
 class _PageState extends State<UseStorageImgPage> {
-  Future<File> _imageFile;
-  bool isVideo = false;
-  VideoPlayerController _controller;
-  VoidCallback listener;
-
-  void _onImageButtonPressed(ImageSource source, int numberOfItems) {
-    setState(() {
-      if (_controller != null) {
-        _controller.setVolume(0.0);
-        _controller.removeListener(listener);
-      }
-      if (isVideo) {
-        ImagePicker.pickVideo(source: source).then((File file) {
-          if (file != null && mounted) {
-            setState(() {
-              _controller = VideoPlayerController.file(file)
-                ..addListener(listener)
-                ..setVolume(1.0)
-                ..initialize()
-                ..setLooping(true)
-                ..play();
-            });
-          }
-        });
-      } else {
-          _imageFile = ImagePicker.pickImages(source: source, numberOfItems: numberOfItems);
-      }
-    });
-  }
-
-  @override
-  void deactivate() {
-    if (_controller != null) {
-      _controller.setVolume(0.0);
-      _controller.removeListener(listener);
-    }
-    super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    if (_controller != null) {
-      _controller.dispose();
-    }
-    super.dispose();
-  }
+  List<Asset> images = List<Asset>();
+  String _error;
 
   @override
   void initState() {
     super.initState();
-    listener = () {
-      setState(() {});
-    };
   }
 
-  Widget _previewVideo(VideoPlayerController controller) {
-    if (controller == null) {
-      return const Text(
-        'You have not yet picked a video',
-        textAlign: TextAlign.center,
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        return AssetView(index, images[index]);
+      }),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    setState(() {
+      images = List<Asset>();
+    });
+
+    List<Asset> resultList;
+    String error;
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 15,
       );
-    } else if (controller.value.initialized) {
-      return Padding(
-        padding: const EdgeInsets.all(10.0),
-        // child: AspectRatioVideo(controller),
-      );
-    } else {
-      return const Text(
-        'Error Loading Video',
-        textAlign: TextAlign.center,
-      );
+    } on PlatformException catch (e) {
+      error = e.message;
     }
-  }
 
-  Widget _previewImage() {
-    return FutureBuilder<File>(
-        future: _imageFile,
-        builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done && snapshot.data != null) {
-            return ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return new Image.file(snapshot.data);
-              },
-              itemCount: 3,
-            );
-          } else if (snapshot.error != null) {
-            return const Text(
-              'Error picking image.',
-              textAlign: TextAlign.center,
-            );
-          } else {
-            return const Text(
-              'You have not yet picked an image.',
-              textAlign: TextAlign.center,
-            );
-          }
-        });
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      if (error == null) _error = 'No Error Dectected';
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('获取本地图片'),
-      ),
-      body: Center(
-        child: isVideo ? _previewVideo(_controller) : _previewImage(),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: () {
-              isVideo = false;
-              _onImageButtonPressed(ImageSource.gallery, 1);
-            },
-            heroTag: 'image0',
-            tooltip: 'Pick Image from gallery',
-            child: const Icon(Icons.photo_library),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                isVideo = false;
-                _onImageButtonPressed(ImageSource.gallery, 2);
-              },
-              heroTag: 'image1',
-              tooltip: 'Pick Image from gallery',
-              child: const Icon(Icons.photo_album),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Center(child: Text('Error: $_error')),
+            RaisedButton(
+              child: Text("Pick images"),
+              onPressed: loadAssets,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                isVideo = false;
-                _onImageButtonPressed(ImageSource.camera, 1);
-              },
-              heroTag: 'image2',
-              tooltip: 'Take a Photo',
-              child: const Icon(Icons.camera_alt),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              backgroundColor: Colors.red,
-              onPressed: () {
-                isVideo = true;
-                _onImageButtonPressed(ImageSource.gallery, 1);
-              },
-              heroTag: 'video0',
-              tooltip: 'Pick Video from gallery',
-              child: const Icon(Icons.video_library),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              backgroundColor: Colors.red,
-              onPressed: () {
-                isVideo = true;
-                _onImageButtonPressed(ImageSource.camera, 1);
-              },
-              heroTag: 'video1',
-              tooltip: 'Take a Video',
-              child: const Icon(Icons.videocam),
-            ),
-          ),
-        ],
+            Expanded(
+              child: buildGridView(),
+            )
+          ],
+        ),
       ),
     );
   }
