748acfe16ea608c32d99616727cce49c4a66c72f
zhongmingkun <v-zhongmingkun@meizu.com>
2019-03-05 17:59:15 +0800

    feat: 自定义onEnd

---
+++ hello_world/lib/components/custom_video_player.dart
@@ -0,0 +1,67 @@
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+class CustomVideoPlayer extends StatefulWidget {
+  final onEnd;
+
+  CustomVideoPlayer({Key key, this.onEnd});
+  @override
+  _CustomVideoPlayerState createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  List<String> _videoList = ['assets/video/1.mp4', 'assets/video/2.mp4'];
+  VideoPlayerController _controller;
+  int _index = 0;
+  Future _delay;
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(
+        _videoList[_index],)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+          _controller.play();
+          _delay = Future.delayed(
+            Duration(milliseconds: _controller.value.duration.inMilliseconds),
+            _onEnd
+          );
+        });
+      });
+  }
+  void _onEnd() {
+    setState(() {
+      int _newIndex = _index + 1;
+      print(_newIndex);
+      print(_index);
+      _index = _newIndex >= _videoList.length ? 0 : _newIndex;
+      print(_index);
+      _controller = VideoPlayerController.asset(
+        _videoList[_index],)
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+          _controller.play();
+          _delay = Future.delayed(
+            Duration(milliseconds: _controller.value.duration.inMilliseconds),
+            _onEnd
+          );
+        });
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _controller.value.initialized
+              ? VideoPlayer(_controller)
+              : Container()
+      );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
--- hello_world/lib/pages/video_player_demo.dart
+++ hello_world/lib/pages/video_player_demo.dart
@@ -1,40 +1,26 @@
-import 'package:video_player/video_player.dart';
+// import 'package:video_player/video_player.dart';
+import '../components/custom_video_player.dart';
 import 'package:flutter/material.dart';
-
 class VideoDemoPage extends StatefulWidget {
   @override
   _VideoDemoPageState createState() => _VideoDemoPageState();
 }
 
 class _VideoDemoPageState extends State<VideoDemoPage> {
-  VideoPlayerController _controller;
-  Duration _position;
+  var _videoPlayer;
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset(
-        'assets/video/1.mp4',)
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {
-          _controller.play();
-          _position =_controller.value.position;
-        });
-      });
+    _videoPlayer = CustomVideoPlayer();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: _controller.value.initialized
-              ? VideoPlayer(_controller)
-              : Container()
-      );
+    return _videoPlayer;
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
   }
 }
